67b2ae28e33bfd27b54cd945954515298566a430
ComfyClicks <79934542+ComfyClicks@users.noreply.github.com>
2024-05-08 17:22:57 -0400

    solved findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
 
+
+const findTheOldest = function(arr) {
+  return arr.reduce((acc, curr) => {
+    const currentYear = new Date().getFullYear();
+    let accAge = acc.yearOfDeath ? acc.yearOfDeath - acc.yearOfBirth : currentYear - acc.yearOfBirth;
+    let currAge = curr.yearOfDeath ? curr.yearOfDeath - curr.yearOfBirth : currentYear - curr.yearOfBirth;
+    return currAge > accAge ? curr : acc;
+  });
 };
+
+
+console.log(findTheOldest(people));
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
