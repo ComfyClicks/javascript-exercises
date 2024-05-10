747b267fa3e11a71acaf6d051b5c4bddba1202a1
ComfyClicks <79934542+ComfyClicks@users.noreply.github.com>
2024-05-09 22:18:42 -0400

    solved some more problems

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+// const repeatString = function(string, num) {
+//   let result = '';
+//   if (num < 0) return 'ERROR'; 
+//   while (num > 0) {
+//     result += string;
+//     num--;
+//   }
+//   return result;
+// };
 
-};
+
+const repeatString = (string, num) => {
+  if (num < 0) return 'ERROR';
+  return string.repeat(num);
+}
 
 // Do not edit below this line
 module.exports = repeatString;
